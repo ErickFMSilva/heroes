acb7afca94818806a4dc72aaaca5d7a34ddc9e0e
Erick Silva <erickferreirasilva@hotmail.com>
2020-07-10 18:23:53 -0300

    Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { InMemoryDataService } from './in-memory-data.service';
+import { MessagesComponent } from './messages/messages.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroAddComponent } from './hero-add/hero-add.component';
+import { HeroFilterPipe } from './hero-filter.pipe';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    NavbarComponent,
+    DashboardComponent,
+    HeroAddComponent,
+    HeroFilterPipe,
+    SearchInputComponent,
+    HeroSearchComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    NgbCollapseModule,
+    HttpClientModule,
+    environment.production
+      ? []
+      : HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+          delay: 500,
+        }),
+    NgbModule,
+  ],
+  providers: [InMemoryDataService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
